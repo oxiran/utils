06617a8f6dc27391363ebe53dce02f4cdbe27231
廖浩然 <413391153@qq.com>
2022-11-18 15:25:29 +0800

    feat: :sparkles: Export formatDate and localStorage

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export { formatDate } from './utils/formatDate';
 export { getDataType } from './utils/getDataType';
+export { setLocalStorage, getLocalStorage } from './utils/localStorage';
 export { toFixedNumber } from './utils/toFixedNumber';
