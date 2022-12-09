de0d22fbe3df7663a100da3b6218589d9409a46c
廖浩然 <413391153@qq.com>
2022-12-09 15:15:06 +0800

    test: :white_check_mark: Add a test for throttle

---
+++ test/utils/throttle.test.ts
@@ -0,0 +1,59 @@
+import { throttle } from '../../src/utils/throttle';
+
+
+describe('Test throtte function', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  test('Different input for throttle function', () => {
+    expect(() => (throttle('hello' as any))).toThrow('Expected a function');
+
+    expect(() => (throttle(20 as any))).toThrow('Expected a function');
+
+    const fn = jest.fn();
+    const throtteFn = throttle(fn, 0);
+
+    throtteFn();
+    expect(fn).toHaveBeenCalled();
+  });
+
+  test('Has been invoked between "delay" time', () => {
+    const fn = jest.fn();
+    const throtteFn = throttle(fn, 100);
+
+    throtteFn();
+    jest.runAllTimers();
+    expect(fn).toHaveBeenCalledTimes(0);
+
+    setTimeout(() => {
+      throtteFn();
+    }, 90);
+    jest.runAllTimers();
+    expect(fn).toHaveBeenCalledTimes(0);
+
+    setTimeout(() => {
+      throtteFn();
+    }, 110);
+    jest.runAllTimers();
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  test('Test invoked in a long time', () => {
+    const fn = jest.fn();
+    const throtteFn = throttle(fn, 50);
+
+    let i: number;
+    for (i = 0; i < 100; i += 1) {
+      setTimeout(() => {
+        throtteFn();
+      }, i * 50);
+    }
+    jest.runAllTimers();
+    expect(fn).toHaveBeenCalledTimes(i - 1);
+  });
+});
